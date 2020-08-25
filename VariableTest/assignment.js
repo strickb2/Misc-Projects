68e5f85cb95597bb562e7e367ad94b583e85ac4c
strickb2 <ben.strickland2@mail.dcu.ie>
2020-08-25 17:28:47 +0100

    Add existing file

---
+++ VariableTest/assignment.js
@@ -0,0 +1,11 @@
+let userInput = 10;
+let userResult;
+
+userResult = 18 + userInput;
+
+userResult = userResult - 5;
+userResult = userResult * 5;
+userResult = userResult / 5;
+
+alert(userResult);
+alert(userInput);
